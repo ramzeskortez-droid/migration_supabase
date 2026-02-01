e377810645bc310eaf7dd0b244fa3f3ec18e2b5d
KortezDmitry <kortez.pc@gmail.com>
2026-02-01 13:08:23 +0300

    FIX: Console buffer size in GEMINI_PROXY_ONLY.bat
    - Added 'mode con: cols=160 lines=9999' to enable scrolling in legacy cmd.exe window.

--- GEMINI_PROXY_ONLY.bat
+++ GEMINI_PROXY_ONLY.bat
@@ -1,7 +1,8 @@
 @echo off
-set HTTP_PROXY=http://w2v4zv:sfJENE@138.219.123.78:9081
-set HTTPS_PROXY=http://w2v4zv:sfJENE@138.219.123.78:9081
-set ALL_PROXY=http://w2v4zv:sfJENE@138.219.123.78:9081
+mode con: cols=160 lines=9999
+set HTTP_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
+set HTTPS_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
+set ALL_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
 set NO_PROXY=localhost,127.0.0.1
-node "F:\NodeJS\node\node_modules\@google\gemini-cli\dist\index.js" %*
+node "%APPDATA%\npm\node_modules\@google\gemini-cli\dist\index.js" %*
 pause
